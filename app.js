0018d81ac404d767ce1d53b2eef9d9bce4f51787
vishnum15 <s.vishnuprasad15@gmail.com>
2020-04-12 14:42:04 +0530

    Creating app

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) => res.send('Hello World!!'))
+const express = require('express');
+const path =require('path');
+//Init app
+const app = express();
+const port = 3000;
+
+// View Engine
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+//Home route 
+app.get('/', (req, res) => res.render('index',{
+    'title':'Hakuna Matata'
+}))
+
+//Add route
+app.get('/users/add',(req,res)=>res.render('add',{arr:[{
+    id:1,
+    name:'onemptied',
+    Place:'Bangalore'
+},{
+    id:2,
+    name:'Twomptied',
+    Place:'Bangalore'
+}]}))
+// Start server
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
